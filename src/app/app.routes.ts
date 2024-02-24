0beea560ded4fe2901360452f293944d6b4dc933
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2024-02-24 16:02:38 -0300

    feat(login): create login module with lazy loading - #02

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const AppRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full', // Note: garante que o nome da rota deva ser idêntico para ser redirecionado
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then((m) => m.LoginModule),
+  },
+];

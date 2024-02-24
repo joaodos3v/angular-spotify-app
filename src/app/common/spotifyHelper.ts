8fcc7e5fd673dfc29f5ddf22bdd6aef216abe8c8
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2024-02-24 19:13:43 -0300

    feat(Helpers): create convertPlaylist common function - #05

--- src/app/common/spotifyHelper.ts
+++ src/app/common/spotifyHelper.ts
@@ -1,9 +1,18 @@
 import { IUser } from 'src/app/interfaces/IUser';
+import { IPlaylist } from 'src/app/interfaces/IPlaylist';
 
 export function convertSportifyUserToCustomUser(user: SpotifyApi.CurrentUsersProfileResponse): IUser {
   return {
     id: user.id,
     name: user.display_name,
     imageUrl: user.images.pop()?.url,
+  };
+}
+
+export function convertSportifyPlaylistToCustomPlaylist(playlist: SpotifyApi.PlaylistObjectSimplified): IPlaylist {
+  return {
+    id: playlist.id,
+    name: playlist.name,
+    imageUrl: playlist.images.pop()?.url,
   };
 }

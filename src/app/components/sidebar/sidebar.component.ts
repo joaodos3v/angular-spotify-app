91348c93556112a52567bf60003e0851438f01cd
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2024-02-24 19:06:11 -0300

    feat(Sidebar): use MenuButton inside Sidebar - #05

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faGuitar, faHome, faMusic, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { MenuButtonComponent } from 'src/app/components/menu-button/menu-button.component';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [MenuButtonComponent, FontAwesomeModule],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent {
+  homeIcon = faHome;
+  searchIcon = faSearch;
+  artistIcon = faGuitar;
+  playlistIcon = faMusic;
 
+  selectedMenu = 'Home';
+
+  constructor() {}
+
+  buttonClick(menuTitle: string) {
+    this.selectedMenu = menuTitle;
+  }
 }

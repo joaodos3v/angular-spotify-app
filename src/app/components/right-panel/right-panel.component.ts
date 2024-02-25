b2d28799b31c3a2c581b071de5f48bef77a1a7c1
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2024-02-25 15:44:42 -0300

    feat(RightPanel): use TopArtists in RightPanel component - #09

--- src/app/components/right-panel/right-panel.component.ts
+++ src/app/components/right-panel/right-panel.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { TopArtistsComponent } from 'src/app/components/top-artists/top-artists.component';
 import { RecentSearchesComponent } from 'src/app/components/recent-searches/recent-searches.component';
 
 @Component({
   selector: 'app-right-panel',
   standalone: true,
-  imports: [RecentSearchesComponent],
+  imports: [RecentSearchesComponent, TopArtistsComponent],
   templateUrl: './right-panel.component.html',
   styleUrl: './right-panel.component.scss',
 })

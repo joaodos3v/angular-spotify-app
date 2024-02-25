f4e52c9e34cf8f07e7ab5a257cd7e4e91a57c780
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2024-02-25 15:14:16 -0300

    feat(RightPanel): use RecentSearches in RightPanel component - #09

--- src/app/components/right-panel/right-panel.component.ts
+++ src/app/components/right-panel/right-panel.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RecentSearchesComponent } from 'src/app/components/recent-searches/recent-searches.component';
 
 @Component({
   selector: 'app-right-panel',
   standalone: true,
-  imports: [],
+  imports: [RecentSearchesComponent],
   templateUrl: './right-panel.component.html',
-  styleUrl: './right-panel.component.scss'
+  styleUrl: './right-panel.component.scss',
 })
-export class RightPanelComponent {
-
-}
+export class RightPanelComponent {}

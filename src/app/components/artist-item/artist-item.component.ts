04c79b02fdd5a8cfeb85421cc03f11f1d6916446
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2024-02-25 15:34:28 -0300

    feat(ArtistItem): create ArtistItem component with original repository styles - #09

---
+++ src/app/components/artist-item/artist-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-artist-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './artist-item.component.html',
+  styleUrl: './artist-item.component.scss'
+})
+export class ArtistItemComponent {
+
+}

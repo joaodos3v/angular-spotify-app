2f7da45a07e7d0075021287bc253d828facda33c
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2024-02-25 15:28:42 -0300

    feat(RecentSearches): finish layout - #09

--- src/app/components/recent-searches/recent-searches.component.ts
+++ src/app/components/recent-searches/recent-searches.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-recent-searches',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './recent-searches.component.html',
   styleUrl: './recent-searches.component.scss',
 })
 export class RecentSearchesComponent {
   recentSearches = ['Top Brasil', 'Top Global', 'Esquenta Sertanejo', 'Funk Hits', 'Pagodeira'];
 
+  searchInput = '';
+
   constructor() {}
+
+  setSearchValue(searchTerm: string) {
+    this.searchInput = searchTerm;
+  }
+
+  doSearch() {
+    /* eslint-disable no-console */
+    console.log('## CL ## Searching...', this.searchInput);
+    // TODO
+  }
 }

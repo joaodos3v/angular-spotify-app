3331be0e1392a2cd00ab6951268a7fe3461b1457
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2024-02-24 21:08:11 -0300

    feat(Player): add router outlet inside player page for children routes - #06

--- src/app/pages/player/player.component.ts
+++ src/app/pages/player/player.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from 'src/app/components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-player',
   standalone: true,
-  imports: [SidebarComponent],
+  imports: [SidebarComponent, RouterOutlet],
   templateUrl: './player.component.html',
   styleUrl: './player.component.scss',
 })
--- src/app/pages/player/player.routes.ts
+++ src/app/pages/player/player.routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router';
 import { PlayerComponent } from './player.component';
+import { HomeComponent } from 'src/app/pages/home/home.component';
 
 export const PlayerRoutes: Routes = [
   {
     path: '',
     component: PlayerComponent,
+    children: [
+      {
+        path: 'home',
+        component: HomeComponent,
+      },
+    ],
   },
 ];

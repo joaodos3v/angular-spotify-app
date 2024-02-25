fbfad8b4eabdca9e1075f204c4c21eb4b17b6b56
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2024-02-24 21:28:16 -0300

    fix(AuthenticatedGuard): fix async execution and inject context - #06

--- src/app/guards/Authenticated.guard.ts
+++ src/app/guards/Authenticated.guard.ts
@@ -2,9 +2,7 @@ import { inject } from '@angular/core';
 import { Router, type CanMatchFn } from '@angular/router';
 import { SpotifyService } from 'src/app/services/spotify.service';
 
-const notAuthenticated = () => {
-  const router = inject(Router);
-
+const notAuthenticated = (router: Router) => {
   localStorage.clear();
   router.navigate(['/login']);
   return false;
@@ -12,19 +10,21 @@ const notAuthenticated = () => {
 
 // Note: impede o carregamento do Módulo até acabar todo o processamento (ex: promise com setTimeout)
 export const AuthenticatedGuard: CanMatchFn = (route, state) => {
+  const router = inject(Router);
+
   const token = localStorage.getItem('token');
   if (!token) {
-    return notAuthenticated();
+    return notAuthenticated(router);
   }
 
   const spotifyService = inject(SpotifyService);
 
-  return new Promise((res) => {
-    const hasCreatedUser = spotifyService.startUser();
+  return new Promise(async (res) => {
+    const hasCreatedUser = await spotifyService.startUser();
     if (hasCreatedUser) {
       res(true);
     } else {
-      res(notAuthenticated());
+      res(notAuthenticated(router));
     }
   });
 };

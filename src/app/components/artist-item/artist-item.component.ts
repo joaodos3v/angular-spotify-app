3e87b4768e65ec722c456e497b7a125b672f8b32
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2024-02-25 15:43:40 -0300

    feat(ArtistItem): finish layout - #09

--- src/app/components/artist-item/artist-item.component.ts
+++ src/app/components/artist-item/artist-item.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-artist-item',
   standalone: true,
   imports: [],
   templateUrl: './artist-item.component.html',
-  styleUrl: './artist-item.component.scss'
+  styleUrl: './artist-item.component.scss',
 })
 export class ArtistItemComponent {
+  @Input()
+  imageSrc = '';
 
+  @Output()
+  click = new EventEmitter<void>();
+
+  onClick() {
+    this.click.emit();
+  }
 }

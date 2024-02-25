7e536aec63e014c7184fc5218fc434c461b123fa
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2024-02-25 16:02:39 -0300

    feat(PlayerCard): use PlayerCard in RightPanel component - #09

--- src/app/components/right-panel/right-panel.component.ts
+++ src/app/components/right-panel/right-panel.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { TopArtistsComponent } from 'src/app/components/top-artists/top-artists.component';
+import { PlayerCardComponent } from 'src/app/components/player-card/player-card.component';
 import { RecentSearchesComponent } from 'src/app/components/recent-searches/recent-searches.component';
 
 @Component({
   selector: 'app-right-panel',
   standalone: true,
-  imports: [RecentSearchesComponent, TopArtistsComponent],
+  imports: [RecentSearchesComponent, TopArtistsComponent, PlayerCardComponent],
   templateUrl: './right-panel.component.html',
   styleUrl: './right-panel.component.scss',
 })

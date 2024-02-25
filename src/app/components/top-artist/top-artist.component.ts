0eb96e3be012c2ddd4f82a1d072208d40ff9c274
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2024-02-24 21:09:03 -0300

    feat(TopArtist): create TopArtist component with original repository styles - #06

---
+++ src/app/components/top-artist/top-artist.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-top-artist',
+  standalone: true,
+  imports: [],
+  templateUrl: './top-artist.component.html',
+  styleUrl: './top-artist.component.scss'
+})
+export class TopArtistComponent {
+
+}

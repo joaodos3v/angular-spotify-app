a7845b55a561a0fe252ac4eed27978dc0dcfc085
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2024-02-25 02:09:55 -0300

    feat(Home): add liked tracks table - #07

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { IMusic } from 'src/app/interfaces/IMusic';
+import { faPlay } from '@fortawesome/free-solid-svg-icons';
+import { SpotifyService } from 'src/app/services/spotify.service';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { TopArtistComponent } from 'src/app/components/top-artist/top-artist.component';
+import { RightPanelComponent } from 'src/app/components/right-panel/right-panel.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [TopArtistComponent],
+  imports: [TopArtistComponent, RightPanelComponent, FaIconComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  musics: IMusic[] = [];
+
+  playIcon = faPlay;
+
+  constructor(private spotifyService: SpotifyService) {
+    this.getMusics();
+  }
+
+  async getMusics() {
+    this.musics = await this.spotifyService.getMusics();
+  }
+
+  getArtists(music: IMusic) {
+    return music.artists.map((artist) => artist.name).join(', ');
+  }
+}

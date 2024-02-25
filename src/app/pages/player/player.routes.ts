2de5e0bc74dbb98d69b116c08531f9bb3d2e1829
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2024-02-25 17:05:35 -0300

    feat(Routes): create playlist route - #10

--- src/app/pages/player/player.routes.ts
+++ src/app/pages/player/player.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { PlayerComponent } from './player.component';
 import { HomeComponent } from 'src/app/pages/home/home.component';
+import { PlaylistComponent } from 'src/app/pages/playlist/playlist.component';
 
 export const PlayerRoutes: Routes = [
   {
@@ -10,6 +11,10 @@ export const PlayerRoutes: Routes = [
       {
         path: 'home',
         component: HomeComponent,
+      },
+      {
+        path: 'playlist/:type/:id',
+        component: PlaylistComponent,
       },
     ],
   },

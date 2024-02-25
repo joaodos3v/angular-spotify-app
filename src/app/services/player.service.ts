457a1b7eb13ffe699212e3617e481ffedfccee57
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2024-02-25 14:42:09 -0300

    feat(PlayerService): add interval to mock an Spotify observer - #08

--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -4,6 +4,8 @@ import { SpotifyService } from './spotify.service';
 import { IMusic } from 'src/app/interfaces/IMusic';
 import { newMusic } from 'src/app/common/factories';
 
+const THREE_SECONDS_IN_MS = 3000;
+
 @Injectable({
   providedIn: 'root',
 })
@@ -11,13 +13,24 @@ export class PlayerService {
   // Note: segundo o instrutor, BehaviorSubject é o mesmo que Subject, com a diferença que conseguimos definir o valor de início
   currentMusic = new BehaviorSubject<IMusic>(newMusic());
 
+  timerId: any = null;
+
   constructor(private spotifyService: SpotifyService) {
     this.getCurrentMusicFromSpotify();
   }
 
+  // Note: criamos esse interval para que fosse possível "reagir" às mudanças feitas no próprio Spotify (como estamos usando a API Rest, não temos como "observar" as mudanças vindas de lá)
   async getCurrentMusicFromSpotify() {
+    clearTimeout(this.timerId);
+
+    // Get current music from Spotify
     const currentMusic = await this.spotifyService.getCurrentMusic();
     this.setCurrentMusic(currentMusic);
+
+    // Create "recursive loop"
+    this.timerId = setInterval(async () => {
+      await this.getCurrentMusicFromSpotify();
+    }, THREE_SECONDS_IN_MS);
   }
 
   setCurrentMusic(music: IMusic) {

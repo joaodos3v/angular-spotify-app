9e7fe3e0cbc39c8005fa0d45cae2b89a4d1768ba
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2024-03-03 20:43:44 -0300

    feat(Repositories): create InjectionToken for all repositories

--- src/app/repositories/artists.repository.ts
+++ src/app/repositories/artists.repository.ts
@@ -1,5 +1,8 @@
+import { InjectionToken } from '@angular/core';
 import { Artist } from 'src/app/models/artist.model';
 
 export interface ArtistsRepository {
   getTopArtists(limit: number): Promise<Artist[]>;
 }
+
+export const ARTISTS_REPOSITORY_PROVIDER = new InjectionToken<ArtistsRepository>('ARTISTS_REPOSITORY_PROVIDER');
--- src/app/repositories/playlists.repository.ts
+++ src/app/repositories/playlists.repository.ts
@@ -1,6 +1,9 @@
+import { InjectionToken } from '@angular/core';
 import { Playlist } from 'src/app/models/playlist.model';
 
 export interface PlaylistsRepository {
   getPlaylists(offfset: number, limit: number): Promise<Playlist[]>;
   getMusicsFromPlaylist(playlistId: string, offset: number, limit: number): Promise<Playlist>;
 }
+
+export const PLAYLISTS_REPOSITORY_PROVIDER = new InjectionToken<PlaylistsRepository>('PLAYLISTS_REPOSITORY_PROVIDER');

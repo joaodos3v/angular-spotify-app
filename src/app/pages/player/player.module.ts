84246b83999eb5ccb3b3b3da7c0c8fbc7fede364
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2024-03-03 21:01:07 -0300

    refact(Providers): remove all player.module providers

--- src/app/pages/player/player.module.ts
+++ src/app/pages/player/player.module.ts
@@ -1,17 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PlayerComponent } from './player.component';
 import { RouterModule } from '@angular/router';
 import { PlayerRoutes } from './player.routes';
-import { ArtistsService } from 'src/app/services/artists.service';
-import { SpotifyPlayerService } from 'src/app/adapters/secondary/spotify/services/spotify-player.service';
-import { SpotifyArtistsRepository } from 'src/app/adapters/secondary/spotify/repositories/spotify-artists.repository';
-import { MusicsService } from 'src/app/services/musics.service';
-import { SpotifyMusicsRepository } from 'src/app/adapters/secondary/spotify/repositories/spotify-musics.repository';
+import { PlayerComponent } from './player.component';
 
 @NgModule({
   declarations: [],
-  providers: [SpotifyPlayerService, SpotifyArtistsRepository, SpotifyMusicsRepository, ArtistsService, MusicsService],
   imports: [CommonModule, RouterModule.forChild(PlayerRoutes), PlayerComponent],
 })
 export class PlayerModule {}
